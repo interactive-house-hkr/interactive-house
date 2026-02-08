762bf8f239917d2127e8e2864d4dea3dabf65b4d
Hugo Nilsson <113201528+HugNil@users.noreply.github.com>
2026-02-08 23:53:44 +0100

    Added middleware (#48)

--- services/src/main.py
+++ services/src/main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
+from services.src.middleware.logging_middleware import logging_middleware
 from services.src.routes.device_routes import router as device_router
 from services.src.routes.state_routes import router as state_router
 from services.src.utils.logger import get_logger
+from fastapi.middleware.cors import CORSMiddleware
 
 logger = get_logger(__name__)
 
 app = FastAPI(title="Interactive House API", version="0.1.0")
 
+app.middleware("http")(logging_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, specify allowed origins
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 def startup_event():
-    logger.info("🚀 Server started")
+    logger.info("Server started")
 
 # Include routers
 app.include_router(device_router, prefix="/api/v1")
--- services/src/services/auth_service.py
+++ services/src/services/auth_service.py
@@ -1,4 +1,5 @@
+from fastapi import Header
 
-
-def optional_user(func):
+# If the param is func, it will be mandatory, so we set default to None to make it optional until authentication is implemented
+def optional_user(authorization: str | None = Header(default=None)):
     return None
