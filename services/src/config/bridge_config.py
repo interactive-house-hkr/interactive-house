7a8b2e2694f75956096c36076b4cda3f01f9e3c3
Jagge1 <hellgrenjacob@hotmail.com>
2026-03-12 23:49:10 +0100

    Added the bridge to the code (not functional yet), bridge config and additions to requirements.txt

---
+++ services/src/config/bridge_config.py
@@ -0,0 +1,12 @@
+import os
+
+DEVICE_UUID = os.getenv("DEVICE_UUID", "arduino")
+
+HM10_UUID = os.getenv("HM10_UUID", "0000ffe1-0000-1000-8000-00805f9b34fb")
+CMD_DELAY = float(os.getenv("CMD_DELAY", "0.2"))
+
+USB_BAUD = int(os.getenv("USB_BAUD", "9600"))
+USB_PORT = os.getenv("USB_PORT") or None
+
+HEARTBEAT_INTERVAL = float(os.getenv("HEARTBEAT_INTERVAL", "1"))
+RECONNECT_DELAY = float(os.getenv("RECONNECT_DELAY", "5"))
