f06389e52678f04cd0d9eab1a721ef7b24ce7861
Jacob Hellgren <93216107+Jagge1@users.noreply.github.com>
2026-04-03 15:02:46 +0200

    160 task create unit tests for auth servicepy (#183)
    * Added pytest to requirements and a skeleton for auth_service tests
    
    * Moved test folder outside of src
    
    * successful registration test added

--- services/tests/test_auth_service.py
+++ services/tests/test_auth_service.py
@@ -1,5 +1,31 @@
 import pytest
 from services.src.services import auth_service
+from services.src.schemas.auth_schema import RegisterRequest, LoginRequest
+
+
+"""
+Fixtures to make mock user object data:
+
+- User login
+- User register
+
+returns dictionary of user data
+"""
+
+@pytest.fixture
+def user_login():
+  return LoginRequest(
+    username= "testUser",
+    password= "123456"
+  )
+  
+@pytest.fixture
+def user_register():
+  return RegisterRequest(
+    username= "testUser",
+    email= "test@example.com",
+    password= "123456"
+  )
 
 
 """
@@ -12,8 +38,17 @@ Unit tests for user registration.
 -
 """
 
-def test_successful_registration():
-  pass
+def test_successful_registration(monkeypatch, user_register):
+  monkeypatch.setattr(auth_service.user_store, "get_user_id_by_username", lambda username: None)
+  monkeypatch.setattr(auth_service, "hash_password", lambda password: "hashed")
+  monkeypatch.setattr(auth_service.user_store, "save_user",lambda user_id, user_data: None)
+  monkeypatch.setattr(auth_service.user_store, "save_username", lambda username, user_id: None)
+  monkeypatch.setattr(auth_service, "create_access_token", lambda user_id: "access-token")
+  monkeypatch.setattr(auth_service, "create_refresh_token", lambda user_id: "refresh-token")
+  
+  result = auth_service.register(user_register)
+  assert result["status"] == "success"
+  
 
 def test_registration_fails_if_username_exists():
   pass
