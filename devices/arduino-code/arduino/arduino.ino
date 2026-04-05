d2a2823dd6aaf0594cf14b460679fe39cdf92675
“Simon <atlaspersson@outlook.com>
2026-04-05 17:38:56 +0200

    Updated compatible arduino code for bl connection and response

--- devices/arduino-code/arduino/arduino.ino
+++ devices/arduino-code/arduino/arduino.ino
@@ -7,6 +7,7 @@
 #include <Servo.h>
 #include <ArduinoJson.h>
 
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo doorServo;
 
@@ -24,47 +25,72 @@ String doorState = "closed";
 // ----- Send Connect -----
 // ------------------------
 void sendConnect()
-{ // ------------------------
-  // Send devices one at a time to avoid RAM overflow
-  // ------------------------
+{
+  {
+    StaticJsonDocument<320> d1;
+    d1["type"] = "CONNECT";
+    JsonObject dev1 = d1["devices"]["arduino-1"].to<JsonObject>();
+    dev1["device_uuid"] = "arduino-1";
+    dev1["type"] = "controller";
+    dev1["transport"]["mode"] = "direct";
+    dev1["transport"]["protocol"] = "ble";
+    dev1["transport"]["service"] = "HMSoft";
+    dev1["status"]["connected"] = true;
+    serializeJson(d1, Serial);
+    Serial.println();
+  } // d1 freed here
+  delay(150);
 
-  // Controller
-  StaticJsonDocument<128> d1;
-  d1["type"] = "CONNECT";
-  d1["device_uuid"] = "arduino-1";
-  d1["device_type"] = "controller";
-  serializeJson(d1, Serial);
-  Serial.println();
-  delay(100);
+  {
+    StaticJsonDocument<320> d2;
+    d2["type"] = "CONNECT";
+    JsonObject dev2 = d2["devices"]["light-1"].to<JsonObject>();
+    dev2["device_uuid"] = "light-1";
+    dev2["type"] = "light";
+    dev2["transport"]["mode"] = "via_controller";
+    dev2["transport"]["controller_uuid"] = "arduino-1";
+    dev2["transport"]["port"] = 13;
+    dev2["capabilities"]["power"]["type"] = "boolean";
+    dev2["capabilities"]["power"]["writable"] = true;
+    dev2["state"]["power"] = lightPower;
+    serializeJson(d2, Serial);
+    Serial.println();
+  } // d2 freed here
+  delay(150);
 
-  // Light
-  StaticJsonDocument<128> d2;
-  d2["type"] = "CONNECT";
-  d2["device_uuid"] = "light-1";
-  d2["device_type"] = "light";
-  d2["power"] = lightPower;
-  serializeJson(d2, Serial);
-  Serial.println();
-  delay(100);
+  {
+    StaticJsonDocument<320> d3;
+    d3["type"] = "CONNECT";
+    JsonObject dev3 = d3["devices"]["door-1"].to<JsonObject>();
+    dev3["device_uuid"] = "door-1";
+    dev3["type"] = "door";
+    dev3["transport"]["mode"] = "via_controller";
+    dev3["transport"]["controller_uuid"] = "arduino-1";
+    dev3["transport"]["port"] = 9;
+    dev3["capabilities"]["open"]["type"] = "string";
+    dev3["capabilities"]["open"]["writable"] = true;
+    dev3["state"]["open"] = doorState;
+    serializeJson(d3, Serial);
+    Serial.println();
+  } // d3 freed here
+  delay(150);
 
-  // Door
-  StaticJsonDocument<128> d3;
-  d3["type"] = "CONNECT";
-  d3["device_uuid"] = "door-1";
-  d3["device_type"] = "door";
-  d3["open"] = doorState;
-  serializeJson(d3, Serial);
-  Serial.println();
-  delay(100);
-
-  // Fan
-  StaticJsonDocument<128> d4;
-  d4["type"] = "CONNECT";
-  d4["device_uuid"] = "fan-1";
-  d4["device_type"] = "fan";
-  d4["power"] = fanPower;
-  serializeJson(d4, Serial);
-  Serial.println();
+  {
+    StaticJsonDocument<320> d4;
+    d4["type"] = "CONNECT";
+    JsonObject dev4 = d4["devices"]["fan-1"].to<JsonObject>();
+    dev4["device_uuid"] = "fan-1";
+    dev4["type"] = "fan";
+    dev4["transport"]["mode"] = "via_controller";
+    dev4["transport"]["controller_uuid"] = "arduino-1";
+    dev4["transport"]["port"] = 6;
+    dev4["capabilities"]["power"]["type"] = "boolean";
+    dev4["capabilities"]["power"]["writable"] = true;
+    dev4["state"]["power"] = fanPower;
+    serializeJson(d4, Serial);
+    Serial.println();
+  } // d4 freed here
+  delay(150);
 }
 
 // --------------------------
